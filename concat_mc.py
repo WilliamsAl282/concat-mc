b9462145bb7f017179754c15bd45bc8787219c93
WilliamsAl282 <WilliamsAl282@northwested.org>
2024-09-20 10:15:41 -0400

    Did the work

---
+++ concat_mc.py
@@ -0,0 +1,34 @@
+# Alex Williams
+# 9/20/2024
+# concatenation check
+
+
+
+# Task 1
+first_name = 'Alexander'
+fav_food = 'burger'
+sentence = "Hi, my name is" + " " + first_name + " " + "and my favorite food is a" + " " + fav_food
+print(sentence)
+
+
+
+
+
+# Task 2
+print('I have learned that concatenation can be used to make sentences.')
+print('I have learned how to properly use concatenation to make sentences and add variables to those sentences.')
+print('I have learned how to properly use numbers with concatenation')
+
+
+
+
+
+# Task 3
+
+
+whopper_price = 4
+
+print('I ordered a whopper from burger king and it cost $' + str(whopper_price))
+
+
+
